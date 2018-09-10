7501e7310ba6e062f80d34c86fac4d2fb67d31ac
mdm121 <milkiles.morrison@gmail.com>
2018-09-10 16:15:58 -0500

    Final Complete

--- apps.js
+++ apps.js
@@ -1,15 +1,20 @@
 let friends = ["Robert", "Taryn", "Timon", "Alicia", "Akeia"];
 let lines;
+let count = 98;
 
 for (i = 0; i < friends.length; i++) {
     console.log(friends[i].toUpperCase() + ":");
 
-    for (lines = 99; lines > 1; lines--) {
-        console.log(lines + " lines of code in the file, " + lines + " lines of code; "
-        + friends[i] + " strikes one out, clears it all out, " + lines + " lines of code in the file.");
+    for (lines = 99; lines > 2; lines--) {
+        console.log(lines + " lines of code in the file, " + lines + " lines of code;  " + friends[i] + " strikes one out, clears it all out, " + count-- + " lines of code in the file.");
     };
-
+    
+    if (lines == 2) {
+        console.log(lines + " lines of code in the file, " + lines + " lines of code; " + friends[i] + " strikes one out, clears it all out, 1 line of code in the file.");
+    }
 
     console.log("1 line of code in the file, 1 line of code; "
     + friends[i] + " strikes one out, clears it all out, no more lines of code in the file.");
+
+    count = 98;
 };
