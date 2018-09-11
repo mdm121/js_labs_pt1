f05759dc241b4e686d5e8da20903d22ca526f1bd
mdm121 <milkiles.morrison@gmail.com>
2018-09-11 10:56:54 -0500

    Optimized

--- apps.js
+++ apps.js
@@ -1,20 +1,16 @@
 let friends = ["Robert", "Taryn", "Timon", "Alicia", "Akeia"];
-let lines;
-let count = 98;
 
 for (let i = 0; i < friends.length; i++) {
     console.log(friends[i].toUpperCase() + ":");
 
-    for (lines = 99; lines > 2; lines--) {
-        console.log(lines + " lines of code in the file, " + lines + " lines of code;  " + friends[i] + " strikes one out, clears it all out, " + count-- + " lines of code in the file.");
+    for (let j = 99; j > 0; j--) {
+        if (j === 2) {
+            console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, 1 line of code in the file.");
+        } else if (j === 1) {
+            console.log("1 line of code in the file, 1 line of code; "
+            + friends[i] + " strikes one out, clears it all out, no more lines of code in the file.");
+        } else {
+            console.log(j + " lines of code in the file, " + j + " lines of code;  " + friends[i] + " strikes one out, clears it all out, " + (j-1) + " lines of code in the file.");  
+        }
     };
-    
-    if (lines == 2) {
-        console.log(lines + " lines of code in the file, " + lines + " lines of code; " + friends[i] + " strikes one out, clears it all out, 1 line of code in the file.");
-    }
-
-    console.log("1 line of code in the file, 1 line of code; "
-    + friends[i] + " strikes one out, clears it all out, no more lines of code in the file.");
-
-    count = 98;
 };
